3acc2a3fa6e89637cc469eafbdd8d5fa43271aba
MAGHC <windowsapple95@gmail.com>
2023-07-14 20:04:47 +0900

    fix: styled.d.ts  세부정의 수정

--- styled.d.ts
+++ styled.d.ts
@@ -3,9 +3,29 @@ import 'styled-components';
 declare module 'styled-component' {
   export interface DefaultTheme {
     colors: {
-      primary: string;
-      secondary: string;
-      secondaryHighlight: string;
+      primary100: string;
+      primary90: string;
+      primary80: string;
+      primary70: string;
+      primary60: string;
+      primary50: string;
+      primary40: string;
+      primary30: string;
+      primary20: string;
+      primary10: string;
+      primary5: string;
+      grayscale100: string;
+      grayscale90: string;
+      grayscale80: string;
+      grayscale70: string;
+      grayscale60: string;
+      grayscale50: string;
+      grayscale40: string;
+      grayscale30: string;
+      grayscale20: string;
+      grayscale10: string;
+      grayscale5: string;
+      meaningfulColor: string;
     };
   }
 }

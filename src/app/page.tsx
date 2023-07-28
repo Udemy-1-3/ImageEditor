a6a8e5b1da9eea7b14ba6f824813d1ba1c2d3512
SeoJiWon1 <dhomp7730@gmail.com>
2023-07-28 15:06:47 +0900

    feat: page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
-'use client';
-
-
-import styled from 'styled-components';
-import CheckBox from "./components/CheckBox";
-import Chip from './components/Chip';
+"use client";
 import React, { useState } from 'react';
-import Tab from './components/Tab';
-
+import styled from 'styled-components';
+import MainModal from './components/imageInsert/mainModal';
+import NewModal from './components/imageInsert/newModal';
 
 const StyledButton = styled.p`
   background-color: #007bff;
@@ -22,20 +18,65 @@ const StyledButton = styled.p`
   }
 `;
 
-const TabContainer = styled.div`
+const Block = styled.div`
   display: flex;
-  gap: 20px;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 20%;
+  background-color: #f0f0f0;
+  border: 1px solid #000;
 `;
 
+const AddButton = styled.button`
+  background-color: #007bff;
+  color: #fff;
+  font-size: 24px;
+  padding: 10px;
+  border: none;
+  border-radius: 50%;
+  cursor: pointer;
+`;
 
-
-
+const ImagePreview = styled.img`
+  width: 100px;
+  height: 100px;
+`;
 
 export default function Home() {
+  const [newModalOpen, setNewModalOpen] = useState(false);
+  const [selectedBlock, setSelectedBlock] = useState<number | null>(null);
+  const [selectedImageData, setSelectedImageData] = useState<string | null>(null);
+
+  const handleNewModalToggle = () => {
+    setNewModalOpen((prevState) => !prevState);
+  };
+
+  const handleNewModalClose = () => {
+    setNewModalOpen(false);
+  };
+
+  const handleNewModalConfirm = (blockId: number, imageData: string) => {
+    setSelectedBlock(blockId);
+    setSelectedImageData(imageData);
+    handleNewModalClose();
+  };
 
   return (
     <>
-      <StyledButton>테스트</StyledButton>
+      <StyledButton onClick={handleNewModalToggle}>테스트</StyledButton>
+      <Block>
+        <AddButton onClick={handleNewModalToggle}>+</AddButton>
+        {selectedBlock !== null && selectedImageData !== null && (
+          <ImagePreview src={selectedImageData} alt={`Block ${selectedBlock}`} />
+        )}
+      </Block>
+      <NewModal
+        newModalOpen={newModalOpen}
+        toggleNewModal={handleNewModalToggle}
+        selectedBlock={selectedBlock}
+        onConfirm={handleNewModalConfirm}
+      />
     </>
   );
 }
